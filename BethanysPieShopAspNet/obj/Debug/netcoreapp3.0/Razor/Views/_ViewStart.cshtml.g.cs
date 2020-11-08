99d8977b33db4ba9fbbca5ee00b54abf9b02baca
kev0h1 <45684757+kev0h1@users.noreply.github.com>
2020-11-08 15:57:35 +0000

    wwwroot

--- BethanysPieShopAspNet/obj/Debug/netcoreapp3.0/Razor/Views/_ViewStart.cshtml.g.cs
+++ BethanysPieShopAspNet/obj/Debug/netcoreapp3.0/Razor/Views/_ViewStart.cshtml.g.cs
@@ -12,7 +12,15 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "C:\Users\Kevin Maingi\source\repos\BethanysPieShopAspNet\BethanysPieShopAspNet\Views\_ViewImports.cshtml"
+using BethanysPieShopAspNet.ViewModels;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7091c65830b0329e613be026ede8a57552863778", @"/Views/_ViewStart.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"e11d4d0157144503d6a5c854c4892391a4eaf4fc", @"/Views/_ViewImports.cshtml")]
     public class Views__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
